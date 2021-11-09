7b4b066b79808afb1a9aa72535f39d122b711f7d
Aldair André <aldairandre99@gmail.com>
2021-11-09 20:22:16 +0100

    add comentarios dos metodos appendFile e writeFIle

--- gerenciandoFileSis/semInterface/app.js
+++ gerenciandoFileSis/semInterface/app.js
@@ -1,6 +1,10 @@
 const appendFile = require('./modulos/appendFile')
 const writeFile = require('./modulos/writeFile')
+const open = require('./modulos/open')
 
 
-appendFile('arq1.txt','Eu sou um arquivo apendado') //cria um arquivo novo
-writeFile('arq2.txt','O conteudo antido deste arquivo foi alterado por este') 
+appendFile('arq1.txt','Eu sou um arquivo apendado') //cria um arquivo novo com conteudo
+
+writeFile('arq2.txt','O conteudo antido deste arquivo foi alterado por este') //Sobre escrevendo um arquivo ja existente 
+
+
