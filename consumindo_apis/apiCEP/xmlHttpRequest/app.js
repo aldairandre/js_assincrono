72586143eb47506e0984c9c4533b22bc96fee373
Aldair André <aldairandre99@gmail.com>
2021-11-16 19:18:30 +0100

    concluido

--- consumindo_apis/apiCEP/xmlHttpRequest/app.js
+++ consumindo_apis/apiCEP/xmlHttpRequest/app.js
@@ -1,7 +1,7 @@
 document.getElementById('btn-cep').addEventListener('click', buscaCep)
 
 function buscaCep() {
-    let cepClient = document.getElementById('cepCliente').value
+    let cepClient = document.getElementById('cepCliente').value.replace(/\D/g, '')
 
     const url = `https://viacep.com.br/ws/${cepClient}/json/`
 
@@ -11,20 +11,30 @@ function buscaCep() {
      * Corpo da requisicao 
      */
     if(!(cepClient == "")){
-        req.open('GET', url, true)
 
-        req.onload = () => {
-            let responseAPI = JSON.parse(req.response)
+        //Expressão regular para validar o CEP.
+        
+        const validacep = /^[0-9]{8}$/;
 
-            document.getElementById('rua').value = (responseAPI.logradouro);
-            document.getElementById('bairro').value = (responseAPI.bairro);
-            document.getElementById('cidade').value = (responseAPI.localidade);
-            document.getElementById('uf').value = (responseAPI.uf);
-        }
-
-        req.send()
-    }else {
-        alert("Erro")
-    }
+        if(validacep.test(cepClient)){
+            req.open('GET', url, true)
     
+            req.onload = () => {
+                let responseAPI = JSON.parse(req.response)
+    
+                document.getElementById('rua').value = (responseAPI.logradouro);
+                document.getElementById('bairro').value = (responseAPI.bairro);
+                document.getElementById('cidade').value = (responseAPI.localidade);
+                document.getElementById('uf').value = (responseAPI.uf);
+            }
+    
+            req.send()
+        }else {
+            alert("Erro")
+        }
+        
+
+    }else{
+        alert('Algo esta errado')
+    }
 }
