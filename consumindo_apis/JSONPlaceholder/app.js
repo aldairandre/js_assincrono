cd14f824fbab7c1f4bb0114cc56275bf6169ab4c
Aldair André <aldairandre99@gmail.com>
2021-11-19 12:40:41 +0100

    add iife

--- consumindo_apis/JSONPlaceholder/app.js
+++ consumindo_apis/JSONPlaceholder/app.js
@@ -3,7 +3,23 @@
  */
 const axios = require('axios')
 
-//Obtendo um recurso
-axios.get('https://jsonplaceholder.typicode.com/posts/1').then((res) => {
-    console.log(res.data);
-})
+const url = 'https://jsonplaceholder.typicode.com/posts/1'
+
+function mostraDados(response){
+    for(conteudo in response){
+        console.log(`${conteudo}: ${response[conteudo]}`);
+    }
+}
+
+const get = (async function() {
+
+    //Corpo da requisicao
+    const response = await axios.get(url).then(response => {
+        return response.data
+    }).catch(err => {
+        console.error(err)
+    });
+    mostraDados(response)
+})()
+
+console.log(get);
